765ecc0bceb0dfc2bb9710af386cb85f7773f28b
Miguel Buitrago <miguelcatholic@gmail.com>
2024-04-21 12:29:42 -0500

    subiendo nuevos cambios

---
+++ backend_con_spring/src/main/java/com/example/backend_con_spring/service/ProductoService.java
@@ -0,0 +1,43 @@
+package com.example.backend_con_spring.service;
+
+import com.example.backend_con_spring.entity.Producto;
+import com.example.backend_con_spring.repository.IProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class ProductoService {
+    @Autowired
+    IProductoRepository productoRepository;
+
+    public ArrayList<Producto> getProductos(){
+        return (ArrayList<Producto>) productoRepository.findAll();
+    }
+
+    public Producto saveProducto(Producto producto){
+        return productoRepository.save(producto);
+    }
+
+    public Optional<Producto>getById(Long id){
+        return productoRepository.findById(id);
+    }
+    public Producto updateById(Producto request, Long id){
+        Producto producto = productoRepository.findById(id).get();
+        producto.setNombre(request.getNombre());
+        producto.setDescripcion(request.getDescripcion());
+
+        return producto;
+    }
+    public Boolean deleteProducto (Long id){
+        try {
+            productoRepository.deleteById(id);
+            return true;
+        }catch (Exception exception){
+            return false;
+        }
+    }
+}
